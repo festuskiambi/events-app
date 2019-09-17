c640f96a875d22c90ef7da54f9eedd1ddfde282a
festus <kiamfestoh@gmail.com>
2019-09-17 15:49:44 +0300

    add packages

--- app/src/main/java/com/example/eventsapp/base/MainActivity.kt
+++ app/src/main/java/com/example/eventsapp/base/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.eventsapp
+package com.example.eventsapp.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eventsapp.R
 
 class MainActivity : AppCompatActivity() {
 
