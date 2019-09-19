81661d91b55f583f90d98e7b8e32ac87a6261f6f
festus <kiamfestoh@gmail.com>
2019-09-19 13:42:07 +0300

    feature create and save event

--- app/src/main/java/com/example/eventsapp/eventdetail/viewmodel/EventDetailViewModel.kt
+++ app/src/main/java/com/example/eventsapp/eventdetail/viewmodel/EventDetailViewModel.kt
@@ -1,12 +1,37 @@
 package com.example.eventsapp.eventdetail.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.eventsapp.domain.entities.Event
 import com.example.eventsapp.domain.usecases.IEventUseCase
+import com.example.rockstars.common.AppDispatchers
+import kotlinx.coroutines.launch
+import java.util.*
+
 
 /**
  * Created by Festus Kiambi on 9/19/19.
  */
 
-class EventDetailViewModel (private val eventsUseCase: IEventUseCase): ViewModel(){
+class EventDetailViewModel(private val eventsUseCase: IEventUseCase,
+                           private val dispatchers: AppDispatchers) : ViewModel() {
+
+    init {
+
+    }
+
+    fun saveEvent(
+        title: String,
+        startDate: String,
+        endDate: String,
+        startTime: String,
+        endTime: String
+    ) = viewModelScope.launch(dispatchers.io) {
+        val uuid = UUID.randomUUID()
+        val id = uuid.toString()
+        val event = Event(id,title,startDate,endDate,startTime,endTime)
+
+        eventsUseCase.createEvent(event)
+    }
 
 }
