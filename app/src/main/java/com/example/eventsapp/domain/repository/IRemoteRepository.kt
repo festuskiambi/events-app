c1343de770a3430253d985fb9310ad31e6c5c510
festus <kiamfestoh@gmail.com>
2019-09-19 07:46:33 +0300

    add domain layer interfaces

---
+++ app/src/main/java/com/example/eventsapp/domain/repository/IRemoteRepository.kt
@@ -0,0 +1,15 @@
+package com.example.eventsapp.domain.repository
+
+import com.example.eventsapp.domain.entities.Event
+
+/**
+ * Created by Festus Kiambi on 9/19/19.
+ */
+interface IRemoteRepository {
+
+    suspend fun getEvents() : List<Event>
+
+    suspend fun editEvent(id: String)
+
+    suspend fun deleteEvent(event: Event)
+}
